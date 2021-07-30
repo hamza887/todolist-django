8e47b1dde54f830de89c3361f40d70f41392d9c8
hamza887 <hamzabutt887@hotmail.com>
2021-07-30 09:55:02 -0700

    making the structure ready

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import 'bootstrap';
+
 
 function App() {
   return (
     <div className="App">
      <h1>TO-DO List</h1>
-    </div>
+      <div className=" todolist input-group mb-3">
+  <input type="text" className="form-control" placeholder="What's for Today" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
+  <div className="input-group-append">
+    <button className=" btntodolist btn btn-warning btn-lg" type="button">ADD to Do List</button>
+  </div> 
+  </div>
+  </div>
   );
 }
 
