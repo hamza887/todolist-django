89c839e1ea9ce12e89ff58534aac88fde8cf7145
hamza887 <hamzabutt887@hotmail.com>
2021-07-30 11:36:13 -0700

    started working on the backend

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>TO-DO List</h1>
+     <h3>TO-DO List</h3>
       <div className=" todolist input-group mb-3">
   <input type="text" className="form-control" placeholder="What's for Today" aria-label="Recipient's username" aria-describedby="basic-addon2"/>
   <div className="input-group-append">
-    <button className=" btntodolist btn btn-warning btn-lg" type="button">ADD to Do List</button>
+    <button className=" btntodolist btn btn-warning " type="button">ADD to Do List</button>
   </div> 
+  <li>hello from list</li>
   </div>
   </div>
   );
